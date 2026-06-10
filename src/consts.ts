d0db1f69dada3d0f895ef195669d5c20a0f000bb
Neal McSpadden <tamo42@gmail.com>
2026-06-10 15:03:58 -0400

    Schema Unification: Integrate semanticConcepts and sameAs Person properties

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,10 @@
 
 export const SITE_TITLE = 'Neal McSpadden';
 export const SITE_DESCRIPTION = 'Personal Control Plane & Endorsed Portfolio';
+
+export const AUTHOR_SAME_AS = [
+	'https://taxsherpa.com',
+	'https://taxsherpa.com/about_neal',
+	'https://www.linkedin.com/in/neal-mcspadden/',
+	'https://www.youtube.com/@NealMcSpadden'
+];
